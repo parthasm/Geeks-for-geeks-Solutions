d6fa52bdbea2c3d6993e82c835a776d92ee16bc6
Partha Sarathi Mukherjee <parthasm2010@gmail.com>
2016-11-12 14:11:41 -0500

    Create Common.java

---
+++ Arrays/Common.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Common
+{
+    //utiility funcitons begin
+    
+    private static void printAr(int[] ar)
+    {
+        for(int loopI = 0; loopI < ar.length; ++loopI)
+            System.out.print(ar[loopI]+" ");
+        System.out.println();
+    }
+    
+    public static void main(String[] args)
+    {
+        int[] ar2 = {7};
+        int[] ar1 = {9,9,9,9,9,8};
+        
+    }
+    //utiility funcitons end
+
+}
+
